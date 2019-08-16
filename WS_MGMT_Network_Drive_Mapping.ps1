8e8f77d7fa1beb13a5320fc8fd266e5cb5ab35e0
SmartMonitoringGuy <53475067+SmartMonitoringGuy@users.noreply.github.com>
2019-08-16 12:03:09 +0200

    Update WS_MGMT_Network_Drive_Mapping.ps1

--- WS_MGMT_Network_Drive_Mapping.ps1
+++ WS_MGMT_Network_Drive_Mapping.ps1
@@ -12,24 +12,24 @@
 function Write-Log {
      [CmdletBinding()]
      param(
-		 #Parameter Message wird definiert als String und darf weder Null noch Leer sein
+	 #Parameter Message wird definiert als String und darf weder Null noch Leer sein
          [Parameter()]
          [ValidateNotNullOrEmpty()]
          [string]$Message,
 		 
-		 #Parameter Severity wird definiert als String mit einem ValidateSet definiert auf Information, Warning und Error und darf weder Null noch Leer sein
+	 #Parameter Severity wird definiert als String mit einem ValidateSet definiert auf Information, Warning und Error und darf weder Null noch Leer sein
          [Parameter()]
          [ValidateNotNullOrEmpty()]
          [ValidateSet('Information','Warning','Error')]
          [string]$Severity = 'Information'
      )
  
-	 #Mittels pscustomobjectwird ein Array erstellt welches die Zeitangaben beinhaltet sowie die Parameter Message und Severity
+     #Mittels pscustomobjectwird ein Array erstellt welches die Zeitangaben beinhaltet sowie die Parameter Message und Severity
      [pscustomobject]@{
          Time = (Get-Date -f g)
          Message = $Message
          Severity = $Severity
-		 #Diese werden mittels Out-File unter C:\Windows\Logs\BeispielLog.txt abgespeichert und für jeden neuen Logeintrag mittels -append erweitert
+       #Diese werden mittels Out-File unter C:\Windows\Logs\BeispielLog.txt abgespeichert und für jeden neuen Logeintrag mittels -append erweitert
      } | Out-File "C:\Windows\Logs\WSMGMTLog.txt" -Append
  }
 
