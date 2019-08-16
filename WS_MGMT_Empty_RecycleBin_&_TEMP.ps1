2ae0b7523ffb5f808f6b738ec6e2b41b7138a574
SmartMonitoringGuy <53475067+SmartMonitoringGuy@users.noreply.github.com>
2019-08-16 11:50:10 +0200

    Update WS_MGMT_Empty_RecycleBin_&_TEMP.ps1

--- WS_MGMT_Empty_RecycleBin_&_TEMP.ps1
+++ WS_MGMT_Empty_RecycleBin_&_TEMP.ps1
@@ -12,24 +12,24 @@
 function Write-Log {
      [CmdletBinding()]
      param(
-		 #Parameter Message wird definiert als String und darf weder Null noch Leer sein
+	 #Parameter Message wird definiert als String und darf weder Null noch Leer sein
          [Parameter()]
          [ValidateNotNullOrEmpty()]
          [string]$Message,
 		 
-		 #Parameter Severity wird definiert als String mit einem ValidateSet definiert auf Information, Warning und Error und darf weder Null noch Leer sein
+	 #Parameter Severity wird definiert als String mit einem ValidateSet definiert auf Information, Warning und Error und darf weder Null noch Leer sein
          [Parameter()]
          [ValidateNotNullOrEmpty()]
          [ValidateSet('Information','Warning','Error')]
          [string]$Severity = 'Information'
      )
  
-	 #Mittels pscustomobjectwird ein Array erstellt welches die Zeitangaben beinhaltet sowie die Parameter Message und Severity
+     #Mittels pscustomobjectwird ein Array erstellt welches die Zeitangaben beinhaltet sowie die Parameter Message und Severity
      [pscustomobject]@{
          Time = (Get-Date -f g)
          Message = $Message
          Severity = $Severity
-		 #Diese werden mittels Out-File unter C:\Windows\Logs\BeispielLog.txt abgespeichert und für jeden neuen Logeintrag mittels -append erweitert
+       #Diese werden mittels Out-File unter C:\Windows\Logs\BeispielLog.txt abgespeichert und für jeden neuen Logeintrag mittels -append erweitert
      } | Out-File "C:\Windows\Logs\WSMGMTLog.txt" -Append
  }
  
@@ -72,7 +72,7 @@ Else {
 #Anschliesssend im ForEach Code Segment wird mittels Get-PSDrive das Laufwerk eingelesen und der Variabel $Drive hinzugefügt
 ForEach ($Drive in Get-PSDrive -PSProvider FileSystem) {
 	#In PathBin wird der Pfad für den Papierkorb definiert
-    $BinPath = $Drive.Name + ':\$Recycle.Bin'
+        $BinPath = $Drive.Name + ':\$Recycle.Bin'
 	#In diesem Codesegment wird mittels Get-ChildItem die Dateien im Papierkorb eingelesen und mittels Recurse für alle Dateien durchgeführt,  Mittels .AddDays werden 7 Tage abgezogen vom aktuellen Datum
 	$Bin = Get-ChildItem $BinPath -Recurse | Where-Object { $_.LastWriteTime -lt (Get-Date).AddDays(-7) }
 	$BinCount = ($Bin).count
