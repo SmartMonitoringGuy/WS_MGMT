ce00320a2e18933794639274f0bc3af6961a6fb7
SmartMonitoringGuy <53475067+SmartMonitoringGuy@users.noreply.github.com>
2019-08-16 11:31:04 +0200

    Update WS_MGMT_Deactivate_Driver_Update.ps1

--- WS_MGMT_Deactivate_Driver_Update.ps1
+++ WS_MGMT_Deactivate_Driver_Update.ps1
@@ -8,9 +8,35 @@
 
 
 
+#Logging Funktion für PowerShell Skripts
+function Write-Log {
+     [CmdletBinding()]
+     param(
+		 #Parameter Message wird definiert als String und darf weder Null noch Leer sein
+         [Parameter()]
+         [ValidateNotNullOrEmpty()]
+         [string]$Message,
+		 
+		 #Parameter Severity wird definiert als String mit einem ValidateSet definiert auf Information, Warning und Error und darf weder Null noch Leer sein
+         [Parameter()]
+         [ValidateNotNullOrEmpty()]
+         [ValidateSet('Information','Warning','Error')]
+         [string]$Severity = 'Information'
+     )
+ 
+	 #Mittels pscustomobjectwird ein Array erstellt welches die Zeitangaben beinhaltet sowie die Parameter Message und Severity
+     [pscustomobject]@{
+         Time = (Get-Date -f g)
+         Message = $Message
+         Severity = $Severity
+		 #Diese werden mittels Out-File unter C:\Windows\Logs\BeispielLog.txt abgespeichert und für jeden neuen Logeintrag mittels -append erweitert
+     } | Out-File "C:\Windows\Logs\WSMGMTLog.txt" -Append
+ }
+ 
+
 $registryPath = "HKLM:\Software\Microsoft\Windows"
 $name = "ExcludeWUDriversInQualityUpdate"
 $value = "1"
 New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
-#Geschrieben von Patrick Urfer am 18.06.2019 funktioniert wie geplant, der Registrykey ist hier gespeichert: Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\ExcludeWUDriversInQualityUpdate
-
+Write-Log -Message 'WS_MGMT_Deactivate_Driver_Update: Die Aktualisierung der Treiber mittels WUpdate wurde deaktiviert' -Severity Information
+#Der Registrykey ist hier gespeichert: Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\ExcludeWUDriversInQualityUpdate
